869597994748003d16d7ee99bf5ce48e40082460
unknown <sergiowillians1@hotmail.com>
2022-03-08 22:02:43 -0300

    nao consegui terminar

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,39 @@
+
+//exercicio 1
+
+/*let valor = 0
+for(let i = 0; i < 5; i++) {
+  valor += i
+}
+console.log(valor)*/
+
+// R: 10 
+
+// exerciicio 2 
+
+/*let lista = [10, 11, 12, 15, 18, 19, 21, 23, 25, 27, 30]
+for (let numero of lista) {
+  if (numero > 18) {
+		console.log(numero)
+	}
+}*/
+//R: o console impriu tos os numeros maior que 18
+//R: (B): basta tirar o if e inicializar a lista com 0
+
+// exercicio 3
+
+/*const quantidadeTotal = Number(prompt("Digite a quantidade de linhas: "))
+let quantidadeAtual = 0
+while(quantidadeAtual < quantidadeTotal){
+  let linha = ""
+  for(let asteriscos = 0; asteriscos < quantidadeAtual + 1; asteriscos++){
+    linha += "*"
+  }
+  console.log(linha)
+  quantidadeAtual++
+}*/
+// o console impreme 4 linhas com asterisco aparti de 1*,**,***,****
+
+//exerciico de inscrita de codigio 1
+
+ 
